824fd237f66b351754ef5a837a34736e388a8bfe
adamkumlin <kumlinadam99@gmail.com>
2022-10-11 00:37:46 +0200

    create navbar
    create navbar

--- prototype/src/App.js
+++ prototype/src/App.js
@@ -1,22 +1,17 @@
 import './App.css';
-import brown_bridge from "./images/brown_bridge.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>HotelsNOW</h1>
-        <a href="#">About us</a>
-        <a href="#">Language</a>
-        <a href="#">Contact us</a>
-        <a href="#">Sign up</a>
-        <a href="#">Log in</a>
-        <img src={brown_bridge} alt="Rectangle-1"/>
-        <input type="text" placeholder="Destination"/>
-        <input type="text" placeholder="Date"/>
-        <input type="text" placeholder="Guests"/>
-        <input type="text" placeholder="Search"/>
-      </header>
+      <h1>HotelsNOW</h1>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Our Services</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contact us</a></li>
+          <li><a href="#" class="book">Book Now</a></li>
+        </ul>
     </div>
   );
 }
