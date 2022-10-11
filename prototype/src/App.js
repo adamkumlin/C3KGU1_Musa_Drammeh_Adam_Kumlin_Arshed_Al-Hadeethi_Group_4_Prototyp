5267cbbf259e0fa08ceeeba50d96cefb2fbf046b
adamkumlin <kumlinadam99@gmail.com>
2022-10-11 20:24:19 +0200

    create intro section and search section

--- prototype/src/App.js
+++ prototype/src/App.js
@@ -1,17 +1,35 @@
 import './App.css';
+// Importerar CSS.
+
+import NavBar from "./components/NavBar.js";
+// Importerar komponenten "NavBar".
+
+import hotelpool from "./images/hotel_pool.jpg";
+import calendaricon from "./images/calendar_icon.png";
+// Importerar två bilder.
 
 function App() {
   return (
     <div className="App">
-      <h1>HotelsNOW</h1>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Our Services</a></li>
-          <li><a href="#">Blog</a></li>
-          <li><a href="#">Contact us</a></li>
-          <li><a href="#" class="book">Book Now</a></li>
-        </ul>
+      <NavBar/>
+      
+      <div className="Intro">
+        <img src={hotelpool}/>
+        <div className="IntroContent">
+          <p>Your home away from home</p>
+          <p className="IntroSlogan">Where Comfort and Affordability meet</p>
+          <a href="#" className="IntroButton">Explore now</a>
+        </div>
+      </div>
+
+      <div className="Search">
+        <img src={calendaricon}/>
+        <label>Destination<br></br> <input type="text"></input></label>
+        <label>Check in<br></br> <input type="date"></input></label>
+        <label>Check out<br></br> <input type="date"></input></label>
+        <label>Guests<br></br> <input type="number"></input></label>
+        <label>Check availability<br></br> <input type="submit"></input></label>
+      </div>
     </div>
   );
 }
---
+++ prototype/src/components/NavBar.js
@@ -0,0 +1,19 @@
+function NavBar() {
+
+    return(
+        <div className="NavBar">
+            <h1>HotelsNOW</h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Our Services</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact us</a></li>
+                <li><a href="#" className="book">Book Now</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
+// Exporterar komponenten som "NavBar".
