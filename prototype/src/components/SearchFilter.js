69559a705cc23d5dacd35623a8d9135e747bf1d6
Adam Kumlin <kumlinadam99@gmail.com>
2022-10-24 20:17:33 +0200

    comment code

--- prototype/src/components/SearchFilter.js
+++ prototype/src/components/SearchFilter.js
@@ -8,7 +8,7 @@ function SearchFilter(props) {
 // Komponenten tar emot props.
 
     const [checkOutDateDisabled, setCheckOutDateDisabled] = useState(true);
-    // Skapar en state-variabel "checkOutDateDisabled" som är satt till "true".
+    // Skapar en state-variabel "checkOutDateDisabled" som är ändras till "true".
 
     const validRoomAndGuestAmount = new RegExp('^[1-9]+$');
     // Skapar en variabel som innehåller ett regex-uttryck som kontrollerar om användaren har skrivit in ett giltigt antal rum och gäster.
@@ -19,20 +19,20 @@ function SearchFilter(props) {
     let dd = String(todaysDate.getDate());
     let mm = String(todaysDate.getMonth() + 1);
     let yyyy = todaysDate.getFullYear();
-    // Skapar variabler som innehåller dagens datum, applicerar dem på det skapade datumet.
+    // Skapar variabler som innehåller dagens datum, de tillämpas på det skapade datumet ("todaysDate").
 
     if (dd < 10) {
-    // Om dagens datum är mindre än 10.
+    // Om dagens nummer är mindre än 10.
 
       dd = "0" + dd;
-      // Lägger till en nolla framför dagens datum.
+      // Lägger till en nolla framför dagens nummer.
     }
 
     if (mm < 10) {
-    // Om månadens datum är mindre än 10.
+    // Om månadens nummer är mindre än 10.
 
       mm = "0" + mm;
-      // Lägger till en nolla framför månadens datum.
+      // Lägger till en nolla framför månadens nummer.
     }
 
     todaysDate = `${yyyy}-${mm}-${dd}`;
@@ -50,14 +50,15 @@ function SearchFilter(props) {
         // Skapar en funktion som tar emot ett event.
     
           props.setCheckInDate(event.target.value)
-          // Anropar sätter "chooseCheckInDate" till det datum som användaren har valt.
+          // Ändrar "chooseCheckInDate" till det datum som användaren har valt.
     
           setCheckOutDateDisabled(false);
-          // Sätter state-variabeln "disabled" till "false" så att input-elementet blir aktiverat.
+          // Ändrar state-variabeln "checkOutDate" till "false" så att input-elementet blir aktiverat.
         }
 
     function handleClick() {
     // Funktionen "handleClick" körs när användaren klickar på knappen.
+        
         if (!props.searchTerm || props.guests < 1 || !props.checkInDate || !props.checkOutDate) {
         // Om state-variabeln "guests" har ett värde som är mindre än 1 eller om state-variabeln "checkInDate" eller "checkOutDate" är tomma.
 
@@ -74,6 +75,7 @@ function SearchFilter(props) {
          // Om checkInDate är lika med checkOutDate. Alltså om användaren har valt samma datum för in- och utcheckning.
 
          alert("The check in and check out date can't be the same.");
+         // Visar ett felmeddelande.
 
         } else {
         props.setDestination(props.searchTerm);
@@ -84,12 +86,12 @@ function SearchFilter(props) {
     return(
         <div className="SearchFilter">
             <img src={calendaricon} alt="Bild på en kalenderikon"/>
-            <label>Destination<input type="text" name="destination" value={props.searchTerm} onChange={handleChange} placeholder="New York"/></label>
+            <label>Destination<input type="text" name="destination" value={props.searchTerm} onChange={handleChange} placeholder="New York"/>
             <label>Check in<input type="date" name="checkInDate" min={todaysDate} value={props.checkInDate} onChange={handleCheckInDateChange}/></label>
             <label>Check out<input disabled={checkOutDateDisabled} type="date" min={props.checkInDate} name="checkOutDate" value={props.checkOutDate} onChange={(e) => props.setCheckOutDate(e.target.value)}/></label>
             <label>Guests<input type="number" name="guests" value={props.guests} onChange={(e) => props.setGuests(e.target.value)}  placeholder="1"/></label>
             <label>Check availability<button onClick={handleClick}>Search</button></label>
-            {/* Skapar fem input-element, fem etiketter och en knapp. */}
+            {/* Skapar fem input-element, en bild, fem etiketter och en knapp. */}
         </div>
     )
 }
